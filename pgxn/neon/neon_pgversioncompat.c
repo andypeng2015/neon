33f5dee06d1e751fc1cd1a1da80cc03d081cd965
Heikki Linnakangas <heikki@neon.tech>
2024-09-16 13:17:01 +0300

    Fix v14
    The InitMaterializedSRF() compatibility function was a few bricks shy
    of a load.

--- pgxn/neon/neon_pgversioncompat.c
+++ pgxn/neon/neon_pgversioncompat.c
@@ -15,7 +15,22 @@ InitMaterializedSRF(FunctionCallInfo fcinfo, bits32 flags)
 {
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
 	Tuplestorestate *tupstore;
+	MemoryContext old_context,
+				per_query_ctx;
 	TupleDesc	stored_tupdesc;
+
+	/* check to see if caller supports returning a tuplestore */
+	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("set-valued function called in context that cannot accept a set")));
+
+	/*
+	 * Store the tuplestore and the tuple descriptor in ReturnSetInfo.  This
+	 * must be done in the per-query memory context.
+	 */
+	per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+	old_context = MemoryContextSwitchTo(per_query_ctx);
 
 	if (get_call_result_type(fcinfo, NULL, &stored_tupdesc) != TYPEFUNC_COMPOSITE)
 		elog(ERROR, "return type must be a row type");
@@ -24,5 +39,6 @@ InitMaterializedSRF(FunctionCallInfo fcinfo, bits32 flags)
 	rsinfo->returnMode = SFRM_Materialize;
 	rsinfo->setResult = tupstore;
 	rsinfo->setDesc = stored_tupdesc;
+	MemoryContextSwitchTo(old_context);
 }
 #endif
